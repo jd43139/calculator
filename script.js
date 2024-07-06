7bef0a2399eeb921daa6349eafda332f624581c4
jd43139 <109301496+jd43139@users.noreply.github.com>
2024-07-06 17:49:40 +0400

    Added hover effect to the buttons Improved the readability and reduced the lines of JS code

--- script.js
+++ script.js
@@ -1,81 +1,56 @@
 let inputDisplay = document.querySelector('.input')
 let expression = []
-
 let numberButtons = document.querySelector(".numbers").querySelectorAll("button")
 let operation= document.querySelector(".operations").querySelectorAll("button")
-
 let zeroDotBtn = document.querySelector(".zero-dot").querySelectorAll("button")
 let equalBtn = document.querySelector(".equal")
 let clearBtn = document.querySelector("#clear")
-let nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','.']
-let clicked = []
-
-function handleOperations(op){
-    op.classList.add("gray")
-    op.classList.remove("op-bg")
-    clicked.push(op)
-    
-    let lastIndex = clicked.length-1
-    if(clicked.length >1 && (clicked[lastIndex]!=clicked[lastIndex-1])){
-        for(let i = 0; i<clicked.length-1; i++){    
-            clicked[i].classList.remove("gray")
-            clicked[i].classList.add("op-bg")
-
+function handleOperations(){
+    operation.forEach((op) =>{
+        this.classList.add("gray")
+        if(op!=this){
+            console.log("REmoved")
+            op.classList.remove("gray")
         }
-        clicked.splice(0,clicked.length-1)
-    
-    }
-
+    })
     expression[0] = Number(inputDisplay.value)
-    expression[1] = op.textContent
+    expression[1] = this.textContent
     this.blur()
 
 }
 function operationKeys(){
-    let ops = 
-{
-    "+": document.querySelector("#add"),
-    "-": document.querySelector("#subtract"),
-    "*": document.querySelector("#multiply"),
-    "/": document.querySelector("#divide")
-}
+    let ops = {"+": operation[0], "-": operation[1], "*": operation[2], "/": operation[3]}
     for (op in ops){
+        //Highlight only the button satifies the test otherwise remove the highlight
         if(event.key == op){
-             handleOperations(ops[op])
+            ops[op].classList.add("gray")
+            expression[0] = inputDisplay.value
+            expression[1] = ops[op].textContent
+            
+        }else{
+           // console.log(event.key, op)
+            ops[op].classList.remove("gray")
         }    
     }
 }
-
-
 function displayKeys(){
+    let nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','.']
     nums.forEach((num) =>{
         if(event.key == num){
-            
-            if(clicked[0]){
-                clicked[0].classList.remove("gray");
-                clicked[0].classList.add("op-bg");
-            }
-            clicked.splice(0,1)
-            //Clear the input field only when it contains the first operand
-            if(inputDisplay.value == expression[0] || inputDisplay.value == "Seriously?"+'\u{1F620}' ){
+            if(inputDisplay.value == expression[0] || inputDisplay.value == "Seriously?"+'\u{1F620}' || inputDisplay.value == "0" ){
                 inputDisplay.value = ""
-            }
+            }//Clear the input field only when it contains the first operand
            inputDisplay.value+=num
         }
-        
-        
-        
-        
     })
-    
 }
 function display(){
     let number = this.textContent
-    if(clicked[0]){
-        clicked[0].classList.remove("gray");
-        clicked[0].classList.add("op-bg");
-    }
-    clicked.splice(0,1)
+    operation.forEach((op) => {
+        if(op.classList[1] == "gray"){
+            op.classList.remove("gray")
+        }
+    })
     //Clear the input field only when it contains the first operand
     if(inputDisplay.value == expression[0] || inputDisplay.value == "Seriously?"+'\u{1F620}' ){
         inputDisplay.value = ""
@@ -90,10 +65,9 @@ function equal(){
     expression[0] = Number(inputDisplay.value)
     this.blur()
 }
-
 function evaluate(a = 0 ,operation = "+" ,b = 0 ){
     if(operation == "+"){
-        return a + b;
+        return Number(a) + Number(b);
     }
     if(operation == "-"){
         return a - b
@@ -102,20 +76,15 @@ function evaluate(a = 0 ,operation = "+" ,b = 0 ){
         return a * b
     }
     if(operation == "/"){
-        if(b === 0){
-            return "Seriously?"+'\u{1F620}'
-        }
+        if(b === 0)return "Seriously?"+'\u{1F620}'
         return a / b
     }
 }
-function clear(){
-    
-    inputDisplay.value = ""
+function clear(){    
+    inputDisplay.value = "0"
     expression = []
-    console.log("activated")
     this.blur()
 }
-
 window.addEventListener("keydown", displayKeys)
 window.addEventListener("keydown", operationKeys)
 window.addEventListener("keydown", () => {if(event.key == "Enter")equal()})
@@ -127,32 +96,5 @@ numberButtons.forEach((button) => {
     button.addEventListener("click",display)
 })
 equalBtn.addEventListener("click",equal)
-
-operation.forEach((op)=> op.addEventListener("click", ()=> { handleOperations(op) }))
-    
+operation.forEach((op)=> op.addEventListener("click",  handleOperations))    
 clearBtn.addEventListener("click",clear)
-
-
-
-
-/**
- * Problem: Make the calculator calculate
- * Enters any number as a and store it inside an array
- * Click any operation and change its color, store the operation inside the same array 
- * enter the second number as b and delete the first number and store in the same array
- * Click = button and clear the screen and display the result
- * call evaluate function , the parameters a,operation,b are the first,second,last index of the array respectively
- * Display the result of the function in the input field
- */
-
-/**
- * Problem: When an operation button is clicked its color becomes gray, when another button is clicked it becomes gray and the previously clicked button return ti its color
- * Click the button and add to it a class .gray that gives  it background color gray
- * Push the recently clicked button into an array
- * if the array length is greter than 1 loop from the first element to the second last item
- * remove the class gray
- * remove the element from the array
- * 
- 
- */
-
